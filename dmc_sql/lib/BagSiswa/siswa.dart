7f2ca92445df05cdcbdbab9b0bee74b017833be1
Saliim11 <qalbunsaliimbakhri@gmail.com>
2023-07-15 15:10:34 +0700

    menambahkan bagian tampilan List

--- dmc_sql/lib/BagSiswa/siswa.dart
+++ dmc_sql/lib/BagSiswa/siswa.dart
@@ -15,6 +15,15 @@ const List<String> listSemester = <String>[
 ];
 const List<String> listKelas = <String>['Kelas A', 'Kelas B', 'Kelas C'];
 
+List<List<String>> listSiswa = [
+  ['Ahmad Solihun - xxxxxxx013', '2022/2023', 'Semester 1', 'Kelas A'],
+  ['Agita Maharani - xxxxxxx012', '2022/2023', 'Semester 1', 'Kelas C'],
+  ['Ahmad Solihun - xxxxxxx013', '2022/2023', 'Semester 1', 'Kelas A'],
+  ['Hilmy Hofifah - xxxxxxx313', '2021/2022', 'Semester 3', 'Kelas B'],
+  ['Agita Maharani - xxxxxxx012', '2022/2023', 'Semester 1', 'Kelas C'],
+  ['Hilmy Hofifah - xxxxxxx313', '2021/2022', 'Semester 3', 'Kelas B'],
+];
+
 class SiswaPage extends StatefulWidget {
   const SiswaPage({super.key});
 
@@ -32,11 +41,11 @@ class _SiswaPageState extends State<SiswaPage> {
     return Scaffold(
       appBar: AppBarAdmin(page: "s"),
       body: Padding(
-        padding: const EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
                   width: 600,
@@ -73,6 +82,156 @@ class _SiswaPageState extends State<SiswaPage> {
                   });
                 })
               ],
+            ),
+            const SizedBox(height: 10),
+
+            // ======================================================================  List Siswa  ============================================================================
+            Container(
+              height: 570,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                border: Border.all(width: 1.5, color: Colors.black),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "Daftar Siswa - $_selectedItemKelas",
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.filter),
+                            )
+                          ],
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                                side: const BorderSide(color: Colors.black),
+                              ),
+                            ),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => InputSiswaPage()));
+                          },
+                          child: Container(
+                            height: 40,
+                            width: 120,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Input Siswa",
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 10, bottom: 10),
+                      height: 1,
+                      width: double.infinity,
+                      color: Colors.black,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: listSiswa.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          String nama = listSiswa[index][0];
+                          String tahun = listSiswa[index][1];
+                          String semester = listSiswa[index][2];
+                          String kelas = listSiswa[index][3];
+
+                          return Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "   ${index + 1}",
+                                        style: const TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 17,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        "   $nama",
+                                        style: const TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 17,
+                                            fontWeight: FontWeight.bold),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "$tahun      ",
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      Text(
+                                        "$semester      ",
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      Text(
+                                        "$kelas   ",
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                        ),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 10, bottom: 10),
+                                height: 0.5,
+                                width: double.infinity,
+                                color: Colors.black,
+                              )
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             )
           ],
         ),
