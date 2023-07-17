3663af0ba9377172f12341528547beb1acd7c686
Howler Nofelle <hilmy@wahidin.net>
2023-07-17 10:09:59 +0700

    UI inputSiswa done 95%

--- dmc_sql/lib/BagSiswa/inputSiswa.dart
+++ dmc_sql/lib/BagSiswa/inputSiswa.dart
@@ -1,4 +1,5 @@
 import 'package:dmc_sql/AppBar/appBarAdmin.dart';
+import 'package:dmc_sql/BagSiswa/siswa.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -19,169 +20,212 @@ class _InputSiswaPageState extends State<InputSiswaPage> {
       appBar: AppBarAdmin(page: "s"),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child:  Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            const Expanded(
-              flex: 1,
-              child: FractionallySizedBox(
-                widthFactor: 0.18,
-              ),
-            ),
-            const SizedBox(width: 10.0),
             Expanded(
-              flex: 4,
-              child: FractionallySizedBox(
-                widthFactor: 1,
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 1.5, color: Colors.black),
-                    borderRadius: BorderRadius.circular(8.0),
+              child: Row(
+                children: [
+                  const Expanded(
+                    flex: 1,
+                    child: FractionallySizedBox(
+                      widthFactor: 0.18,
+                    ),
                   ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Text(
-                        "Input Siswa",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                      const SizedBox(height: 10.0),
-                      const Text(
-                        "ISI DATA SISWA",
-                        style: TextStyle(
-                            fontSize: 20
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                      const SizedBox(height: 10.0),
-                      Container(
+                  const SizedBox(width: 10.0),
+                  Expanded(
+                    flex: 4,
+                    child: FractionallySizedBox(
+                      widthFactor: 1,
+                      child: Container(
+                        padding: const EdgeInsets.all(20),
                         decoration: BoxDecoration(
+                          border: Border.all(width: 1.5, color: Colors.black),
                           borderRadius: BorderRadius.circular(8.0),
-                          border: Border.all(color: Colors.black, width: 1.5),
                         ),
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Nama siswa',
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.symmetric(
-                              horizontal: 14.0,
-                              vertical: 12.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0),
-                          border: Border.all(color: Colors.black, width: 1.5),
-                        ),
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Nomor Induk Siswa',
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.symmetric(
-                              horizontal: 14.0,
-                              vertical: 12.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Container(
-                              padding: const EdgeInsets.only(left: 10, right: 10),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8.0),
-                                border: Border.all(color: Colors.black, width: 1.5),
-                              ),
-                              child: DropdownButton<String>(
-                                hint: Text("Semester"),
-                                isExpanded: true,
-                                value: selectedOption,
-                                items: const [
-                                  DropdownMenuItem(
-                                    value: 'Ganjil',
-                                    child: Text('Ganjil'),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              alignment: Alignment.topLeft,
+                              child: Column(
+                                children: const [
+                                  Text(
+                                    "Input Siswa",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.left,
                                   ),
-                                  DropdownMenuItem(
-                                    value: 'Genap',
-                                    child: Text('Genap'),
+                                  SizedBox(height: 10.0),
+                                  Text(
+                                    "ISI DATA SISWA",
+                                    style: TextStyle(fontSize: 18),
                                   ),
                                 ],
-                                onChanged: (value) {
-                                  setState(() {
-                                    selectedOption = value;
-                                  });
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                border: Border.all(color: Colors.black, width: 1.5),
+                              ),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'Nama siswa',
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 14.0,
+                                    vertical: 12.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                border: Border.all(color: Colors.black, width: 1.5),
+                              ),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'Nomor Induk Siswa',
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 14.0,
+                                    vertical: 12.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    padding: const EdgeInsets.only(left: 10, right: 10),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      border: Border.all(color: Colors.black, width: 1.5),
+                                    ),
+                                    child: DropdownButton<String>(
+                                      hint: const Text("Semester"),
+                                      isExpanded: true,
+                                      value: selectedOption,
+                                      items: const [
+                                        DropdownMenuItem(
+                                          value: 'Ganjil',
+                                          child: Text('Ganjil'),
+                                        ),
+                                        DropdownMenuItem(
+                                          value: 'Genap',
+                                          child: Text('Genap'),
+                                        ),
+                                      ],
+                                      onChanged: (value) {
+                                        setState(() {
+                                          selectedOption = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 20.0),
+                                Expanded(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      border: Border.all(color: Colors.black, width: 1.5),
+                                    ),
+                                    child: const TextField(
+                                      decoration: InputDecoration(
+                                        hintText: 'Kelas',
+                                        border: InputBorder.none,
+                                        contentPadding: EdgeInsets.symmetric(
+                                          horizontal: 14.0,
+                                          vertical: 12.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 20.0),
+                                Expanded(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      border: Border.all(color: Colors.black, width: 1.5),
+                                    ),
+                                    child: const TextField(
+                                      decoration: InputDecoration(
+                                        hintText: 'Tahun Ajaran',
+                                        border: InputBorder.none,
+                                        contentPadding: EdgeInsets.symmetric(
+                                          horizontal: 14.0,
+                                          vertical: 12.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 20.0),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      side: const BorderSide(color: Colors.black),
+                                    ),
+                                  ),
+                                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => SiswaPage()),
+                                  );
                                 },
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 10.0),
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8.0),
-                                border: Border.all(color: Colors.black, width: 1.5),
-                              ),
-                              child: const TextField(
-                                decoration: InputDecoration(
-                                  hintText: 'Kelas',
-                                  border: InputBorder.none,
-                                  contentPadding: EdgeInsets.symmetric(
-                                    horizontal: 14.0,
-                                    vertical: 12.0,
+                                child: Container(
+                                  height: 50,
+                                  width: 120,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const [
+                                      Text(
+                                        "Selesai",
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                          const SizedBox(width: 10.0),
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8.0),
-                                border: Border.all(color: Colors.black, width: 1.5),
-                              ),
-                              child: const TextField(
-                                decoration: InputDecoration(
-                                  hintText: 'Tahun Ajaran',
-                                  border: InputBorder.none,
-                                  contentPadding: EdgeInsets.symmetric(
-                                    horizontal: 14.0,
-                                    vertical: 12.0,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(width: 20.0),
+                  const Expanded(
+                    flex: 1,
+                    child: FractionallySizedBox(
+                      widthFactor: 0.18,
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(width: 10.0),
-            const Expanded(
-              flex: 1,
-              child: FractionallySizedBox(
-                widthFactor: 0.18,
-              ),
-            ),
+            SizedBox(height: 20.0),
           ],
-        )
-
+        ),
       ),
     );
-
   }
+
 }
