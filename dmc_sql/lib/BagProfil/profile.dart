ad1f79c9fff33a167c064cfcf97519ee7bcaa54d
Howler Nofelle <hilmy@wahidin.net>
2023-07-27 20:21:49 +0700

    login & appBar for user added

--- dmc_sql/lib/BagProfil/profile.dart
+++ dmc_sql/lib/BagProfil/profile.dart
@@ -1,29 +1,11 @@
+import 'package:dmc_sql/AppBar/appBarUser.dart';
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(ProfileApp());
-}
-
-class ProfileApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Profile Template',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ProfilePage(),
-    );
-  }
-}
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile Template'),
-      ),
+      appBar: AppBarUser(page: "p"),
       body: Container(
         padding: const EdgeInsets.all(20.0),
         child: Column(
